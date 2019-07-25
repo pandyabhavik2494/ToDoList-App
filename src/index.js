aec347cfefe77f3370de16c413bec397f2625aef
Bhavik pandya <bhavikpandya@Bhaviks-MacBook-Air.local>
2019-07-25 19:59:27 +0530

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import MainContent from './TodoList';
-  
-var destination = document.querySelector("#container")
-  
-ReactDOM.render(<MainContent/>,destination);
